cfac653a1a1c301c82b2ffaa1be17272b13cef1a
Satish Kumar Yadav <satishkumar.yadav.7549@gmail.com>
2025-09-04 20:32:18 +0530

    initial commit

--- assignment1-render-app/src/App.jsx
+++ assignment1-render-app/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import './app.css';
+import { sculptureList } from "./data";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [index, setIndex] = useState(0)
+  const [showMore, setShowMore] = useState(false)
 
+  //A state variable to retain the data between renders
+  // A state setter function to update variable and trigger React to render the component again 
+
+  function handleClick(){
+    setIndex(index + 1)
+  } // event handler is updating the local variable, index. 
+
+  let sculpture = sculptureList[index];
+  
+
+  function handleMoreClick(){
+    setShowMore(!showMore) // fasle -> true, true -> false
+  }// just another event handler that will help us to show the description 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleClick}>next</button>
+      <h2>
+        <i>{sculpture.name}</i> by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+
+     <p>Click the Image below to get its Description.</p>
+
+       <img  
+        id="image"
+        onClick={handleMoreClick}  
+        src={sculpture.url}
+        alt={sculpture.alt}
+        title="Click to get detailed description"
+      /> 
+    
+
+      <br></br>
+      {/* <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : 'Show'} details
+      </button> */}
+        {showMore && <p>{sculpture.description}</p>}
+        
+        {/* showmore -> true */}
+        {/*conditional rendering */}
     </>
-  )
+  );
 }
 
-export default App
+// show -> true => show the content 
+// showmore -> false => hide the content
+
+export default App;
+
+
+// useState, as well as any other function starting with "use", is called Hook. 
+
+
+// Assignment -> make image clickable to show the description of the image 
+// Post it on github 
